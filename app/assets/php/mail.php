981d380e6029ebe9efaa2410cecd9ea936a03d91
adoonq <adoonq@gmail.com>
2018-12-12 22:35:23 +0200

    update

--- app/assets/php/mail.php
+++ app/assets/php/mail.php
@@ -1,35 +1,53 @@
 <?php
 
-$project_name = '';
-$form_subject = '';
-$admin_email  = '';
+$mailto = 'admin@mail.com';
+$subject = 'Название проекта';
 
 $form_data = array(
 	'Имя'					=> $_POST['name'] ?: 'Не указано',
-	'Телефон' 		=> $_POST['phone'] ?: 'Не указан'
+	'Телефон' 		=> $_POST['phone'] ?: 'Не указан',
 );
 
 $c = true;
-foreach ( $form_data as $key => $value ) {
+foreach ($form_data as $key => $value) {
 	if ( $value != "" ) {
 		$message .= "
 		" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-		</tr>
+			</tr>
 		";
 	}
 }
 
 $message = "<table style='width: 100%;'>$message</table>";
 
-function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+function send_mail($to, $subject, $message, $base64 = false) {
+	$separator = md5(time());
+	$eol = "\r\n";
+
+	$headers = "From: " . $subject . " <" . $to . ">" . $eol;
+	$headers .= "MIME-Version: 1.0" . $eol;
+	$headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+
+	$body = "--" . $separator . $eol;
+	$body .= "Content-Type: text/html; charset=utf-8" . $eol;
+	$body .= "Content-Transfer-Encoding: 8bit" . $eol;
+	$body .= $eol . $message . $eol . $eol;
+
+	if ($base64) {
+		$filename = 'myfile.png';
+		$content = chunk_split(preg_replace('#^data:image/[^;]+;base64,#', '', $base64));
+
+		$body .= "--" . $separator . $eol;
+		$body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+		$body .= "Content-Transfer-Encoding: base64" . $eol;
+		$body .= "Content-Disposition: attachment" . $eol;
+		$body .= $eol . $content . $eol . $eol;
+		$body .= "--" . $separator . "--";
+	}
+
+	return mail($to, $subject, $body, $headers);
 }
 
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-'Reply-To: '.$admin_email.'' . PHP_EOL;
-
-mail($admin_email, adopt($form_subject), $message, $headers);
+send_mail($mailto, $subject, $message);
--- app/javascript/custom/mail.js
+++ app/javascript/custom/mail.js
@@ -1,15 +1,29 @@
 $('form').submit(function(e) {
-	var th = $(this);
+	var form = $(this);
+
+	form.find('button').attr('disabled', true);
 
 	$.ajax({
-		type: "POST",
-		url: "assets/php/mail.php",
-		data: th.serialize()
+		type: 'POST',
+		url: 'assets/php/mail.php',
+		data: form.serialize()
 	})
-	.done(function() {
-		th.trigger('reset');
-	})
-	.fail(function(xhr) {});
+	.done(function() {})
+	.fail(function(xhr) {})
+	.always(function() {
+		form.trigger('reset');
+		form.find('button').attr('disabled', false);
+	});
 
 	return false;
 });
+
+
+// objectifyForm(form.serializeArray())
+function objectifyForm(formArray) {
+	var returnArray = {};
+	for (var i = 0; i < formArray.length; i++){
+		returnArray[formArray[i]['name']] = formArray[i]['value'];
+	}
+	return returnArray;
+}
